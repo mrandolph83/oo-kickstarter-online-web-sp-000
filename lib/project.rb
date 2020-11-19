bfbe46da38fc836ceea233a8cd65471c1cd76564
Matt Smith <mrandolph83@gmail.com>
2020-11-19 00:02:22 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,11 +5,15 @@ class Project
 
   def initialize(title)
     @title = title
+  # has many backers related to the title of the project
     @backers = []
   end
 
   def add_backer(backer)
+  # Adds bacler to the project
     @backers.push(backer)
+    # Adds protect to the backer using the back_project method from the
+   #  Backer class unless they're already on it.
     backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 
